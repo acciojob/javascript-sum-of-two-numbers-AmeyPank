43639141fde1c4507e55672f3fede9623a3f4ab9
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-09-14 13:02:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-let input1 = prompt("Enter the first number:");
-let input2 = prompt("Enter the second number:");
+// Prompt the user for the first number
+const firstNumber = prompt("First number:");
 
-// Write your code here and print the output using alert function
+// Check if the user canceled the prompt
+if (firstNumber === null) {
+  alert("Operation canceled.");
+} else {
+  // Convert the input to a number
+  const num1 = parseFloat(firstNumber);
+
+  // Check if the input is a valid number
+  if (!isNaN(num1)) {
+    // Prompt the user for the second number
+    const secondNumber = prompt("Second number:");
+
+    // Check if the user canceled the prompt
+    if (secondNumber === null) {
+      alert("Operation canceled.");
+    } else {
+      // Convert the input to a number
+      const num2 = parseFloat(secondNumber);
+
+      // Check if the input is a valid number
+      if (!isNaN(num2)) {
+        // Calculate and display the sum
+        const sum = num1 + num2;
+        alert(`The sum of ${num1} and ${num2} is ${sum}.`);
+      } else {
+        // Display an error message for the second input
+        alert("Invalid input. Please enter a valid number.");
+      }
+    }
+  } else {
+    // Display an error message for the first input
+    alert("Invalid input. Please enter a valid number.");
+  }
+}
